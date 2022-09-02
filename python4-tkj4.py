5d35f392cfcdc99a031150dc573f6daf87877090
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-02 14:34:04 +0700

    Add files via upload

---
+++ python4-tkj4.py
@@ -0,0 +1,48 @@
+
+#rumus persegi
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("luas persegi adalah", luas_p)
+
+
+#rumus persegi panjang
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah",luas_pp)
+
+
+#rumus trapesium 1/2 * (a+b) * tinggi
+print("\ntrapesium")
+a = 6
+b = 10 
+t = 8
+luas_t = (a+b) * t /2
+print("luas Trapesium adalah",luas_t)
+
+#rumus segitiga 1/2 * a * t
+print("\nsegitiga")
+a = 20
+t = 10
+luas_s = a * t /2
+print("luas segitiga",luas_s)
+
+#rumus jajar genjang a * t
+print("\njajar genjang")
+a = 30
+t = 12
+luas_j = a * t
+print("luas jajar genjang",luas_j)
+
+#rumus lingkaran 3.14 * r
+print("\nlingkaran")
+r = 200
+luas_i = 3.14 * r
+print("luas ingkaran",luas_i)
+
+
+
+
+
+
