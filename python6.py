e0b83d09964b76edcd7eaa2c8046c3119a3f36cd
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-05 10:04:57 +0700

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,17 @@
+list_buah= ['Pisang', 'Nanas', 'Melon', 'Durian']
+print(list_buah[-1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[-4])
+
+list_nama= ['fadilah', 'keysa yullianti', 'vito', 'asu']
+print(list_nama[-1])
+print(list_nama[-2])
+print(list_nama[-3])
+print(list_nama[-4])
+
+list_nilai = [80, 70, 90, 60]
+print(list_nilai[-1])
+print(list_nilai[-2])
+print(list_nilai[-3])
+print(list_nilai[-4])
