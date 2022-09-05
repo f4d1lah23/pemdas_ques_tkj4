50207cef5d33dafb4da0a4105bc014a5c3a47485
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-05 08:49:20 +0700

    Add files via upload

---
+++ python-TKJ4.py
@@ -0,0 +1,19 @@
+
+#dollar ke rupiah dollar * rupiah
+dollar = 100
+rupiah = 14.895
+idr = dollar * rupiah
+print (" dollar jika dikonversikan kerupiah sebesar",idr)
+
+
+#dollar ke rupiah euro * yen
+euro = 3
+yen = 139,14
+idr = euro * yen
+print (" euro jika dikonversikan keyen sebesar",idr)
+
+#dollar ke rupiah won * rupiah
+won =1
+rupiah =10,92
+idr = won * rupiah
+print (" won jika dikonversikan kerupiah sebesar",idr)
