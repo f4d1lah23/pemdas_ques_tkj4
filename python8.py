62fa40ea2b74b02a950a361cc73f920a2e393763
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-07 13:54:21 +0700

    Add files via upload

---
+++ python8.py
@@ -0,0 +1,60 @@
+list_integer = [1,2,3,100]
+list_string = ["fadil","keysa yullianti","vito"]
+list_mix = [20,"putin",True,"Bray"]
+
+print("Data sebelum diubah:", list_string)
+list_string[0] = "keysa yullianti"
+print("Data setelah diubah:", list_string)
+
+print("Data sebelum diubah:", list_string)
+list_string[2] = "fadil"
+print("Data setelah diubah:", list_string)
+
+print("Data sebelum diubah:", list_mix)
+list_mix[0] = "putin"
+print("Data setelah diubah:", list_mix)
+
+print("Data sebelum diubah:", list_mix)
+list_mix[1] = "20"
+print("Data setelah diubah:", list_mix)
+
+print("Data sebelum diubah:", list_mix)
+list_mix[2] = "Bray"
+print("Data setelah diubah:", list_mix)
+
+print("Data sebelum diubah:", list_mix)
+list_mix[3] = "True"
+print("Data setelah diubah:", list_mix)
+
+
+
+
+
+#perulangan for
+
+list_string = ["Fadil","keysa yullianti","vito"]
+
+for x in list_string:
+    print(x)
+
+print("\n")
+for i in list_string:
+    print(i)
+
+list_integer = [1,2,3,100]
+
+for x in list_integer:
+ print(x)
+
+print("\n")
+for i in list_string:
+    print(i)
+
+list_mix = [20,"putin",True,"Bray"]
+
+for x in list_mix  :
+    print(x)
+
+print("\n")
+for i in list_string:
+    print(i)
