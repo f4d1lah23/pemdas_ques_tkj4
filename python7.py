94ce3ec4776e80ed903d00ffc09dde4f02d6a8be
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-07 08:46:05 +0700

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,25 @@
+list_kosong = []
+list_integer = [1,2,3,100]
+list_string = ["fadil","keysa yullianti","vito"]
+list_mix = [20,"putin",True,"Bray"]
+
+print("List Kosong:", list_kosong)
+print("List Data Interger:", list_integer)
+print("List Data String:", list_string)
+print("List Data Mix:", list_mix)
+
+print("Data kedua list string:", list_string[1])
+print("Data pertama list intiger:", list_integer[0])
+print("Data ketiga list mix:", list_mix[2])
+
+print("Data keempat list string:", list_string[0])
+print("Data kelima list integer:", list_integer[1])
+print("Data pertama list mix:", list_mix[1])
+
+print("Data keempat list string:", list_string[2])
+print("Data kelima list integer:", list_integer[2])
+print("Data kedua list mix:", list_mix[0])
+
+
+print("Data kedua list integer:", list_integer[3])
+print("Data ketiga list mix:", list_mix[3])
