0aab7e1c549156016a600ee47d586394353bf6ad
f4d1lah23 <112687656+f4d1lah23@users.noreply.github.com>
2022-09-09 14:51:43 +0700

    Add files via upload

---
+++ UPRAK_fadilah_10tkj4.py
@@ -0,0 +1,35 @@
+#rumus balok
+panjang = 17
+lebar = 14
+tinggi = 7
+volume_b=panjang*lebar*tinggi
+print("volume balok adalah", volume_b)
+
+
+#rumus kubus
+sisi1 = 20
+sisi2 = 20
+sisi3 = 20
+volume_k = sisi1 * sisi2 * sisi3
+print("volume kubus adalah", volume_k)
+
+#limas = 1/3 * luas_alas * tinggi
+
+luas_alas= 20
+tinggi= 30
+limas= 1/3 * luas_alas * tinggi
+print("volume  dari limas adalah", limas)
+
+
+#tabung 22/7*r*r*t
+
+r=10
+t=7
+Itabung=22/7*r*r*t
+print("volume dari tabung adalah", Itabung)
+
+#celcius to reamur 5/4
+celcius=1000
+reamur=8.8
+rreamur=celcius*reamur
+print("celcius ke reamur", rreamur)
